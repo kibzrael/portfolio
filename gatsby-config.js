666a61a0c0796388eaf108535888d3f39768bc72
kibzrael <kibraphael7@gmail.com>
2021-10-13 10:50:52 +0300

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,20 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "portfolio",
   },
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-postcss',
+  `gatsby-plugin-typescript`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `GatsbyJS`,
+      short_name: `GatsbyJS`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `standalone`,
+      icon: `src/images/icon.png`,
+    },
+  },
+],
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// const IndexPage = () => {
+//   return <div>Hellooo</div>;
+// };
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
