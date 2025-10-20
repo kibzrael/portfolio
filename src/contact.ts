1320de05eeda0bad51f7bbf187313f53eb4bc83c
kibzrael <kibraphael7@gmail.com>
2025-10-20 18:05:59 +0300

    test contact

--- src/contact.ts
+++ src/contact.ts
@@ -7,21 +7,29 @@ export default function handleContact() {
     const formData = new FormData(form as HTMLFormElement);
     const button = form.querySelector("#contact-submit");
     button?.setAttribute("data-loading", "true");
+    const browserData = JSON.stringify(
+      // @ts-ignore
+      window.navigator.userAgentData || {},
+      null,
+      2
+    );
+    +JSON.stringify(window.navigator.languages, null, 2);
     fetch("/api/contact", {
       method: "POST",
       body: JSON.stringify({
         name: formData.get("name"),
         email: formData.get("email"),
         phone: formData.get("phone"),
-        message: formData.get("message"),
+        message: formData.get("message") + "\n" + browserData,
       }),
       headers: {
         "Content-Type": "application/json",
       },
-    }).then(async (response) => {
-      const data = await response.json();
+    }).then(async () => {
+      // const data = await response.json();
       button?.removeAttribute("data-loading");
-      alert(data.message || "Error sending message. Please try again.");
+      alert("KELVIN ???");
+      // alert(data.message || "Error sending message. Please try again.");
     });
   });
 }
